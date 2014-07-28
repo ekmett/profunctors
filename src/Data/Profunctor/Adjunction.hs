58d86cf79cae1430d86b5cf178ac36ff01d9f3d7
Edward Kmett <ekmett@gmail.com>
2014-07-28 11:22:27 -0400

    class (ProfunctorFunctor f, ProfunctorFunctor u) => ProfunctorAdjunction f u | f -> u, u -> f

--- src/Data/Profunctor/Adjunction.hs
+++ src/Data/Profunctor/Adjunction.hs
@@ -5,7 +5,8 @@
 module Data.Profunctor.Adjunction where
 
 import Data.Profunctor
+import Data.Profunctor.Monad
 
-class ProfunctorAdjunction f u | f -> u, u -> f where
+class (ProfunctorFunctor f, ProfunctorFunctor u) => ProfunctorAdjunction f u | f -> u, u -> f where
   unit   :: Profunctor p => p -/-> u (f p)
   counit :: Profunctor p => f (u p) -/-> p
