1243344332f2857dc147c840dc978ddc98184cab
Edward Kmett <ekmett@gmail.com>
2014-07-28 01:17:03 -0400

    -/-> in adjunctions

--- src/Data/Profunctor/Adjunction.hs
+++ src/Data/Profunctor/Adjunction.hs
@@ -1,8 +1,11 @@
-{-# LANGUAGE TypeOperators, MultiParamTypeClasses, FunctionalDependencies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE RankNTypes #-}
 module Data.Profunctor.Adjunction where
 
 import Data.Profunctor
 
 class f -| u | f -> u, u -> f where
-  unit   :: Profunctor p => p a b -> u (f p) a b
-  counit :: Profunctor p => f (u p) a b -> p a b
+  unit   :: Profunctor p => p -/-> u (f p)
+  counit :: Profunctor p => f (u p) -/-> p
