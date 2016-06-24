16d112c52d915cfb9c9f82c1257ce874a5bb31ba
Zemyla <zemyla@gmail.com>
2016-06-24 18:42:20 -0500

    ArrowChoices are Traversing.
    Add functions to implement wander and traverse' for ArrowChoices; also
    make WrappedArrow a Traversing.

--- src/Data/Profunctor/Traversing.hs
+++ src/Data/Profunctor/Traversing.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Data.Profunctor.Traversing
   ( Traversing(..)
   , CofreeTraversing(..)
@@ -12,10 +13,15 @@ module Data.Profunctor.Traversing
   -- * Choice in terms of Traversing
   , leftTraversing
   , rightTraversing
+  -- * Traversing an ArrowChoice
+  , wanderA
+  , traverseA
   ) where
 
-import Control.Applicative
-import Control.Arrow (Kleisli(..))
+import Prelude hiding (id, (.))
+import Control.Category
+import Control.Applicative hiding (WrappedArrow(..))
+import Control.Arrow
 import Data.Functor.Compose
 import Data.Functor.Identity
 import Data.Orphans ()
@@ -25,7 +31,6 @@ import Data.Profunctor.Strong
 import Data.Profunctor.Types
 import Data.Profunctor.Unsafe
 import Data.Traversable
-import Data.Tuple (swap)
 
 #if __GLASGOW_HASKELL__ < 710
 import Data.Monoid (Monoid)
@@ -34,16 +39,13 @@ import Prelude hiding (mapM)
 #endif
 
 firstTraversing :: Traversing p => p a b -> p (a, c) (b, c)
-firstTraversing = dimap swap swap . traverse'
+firstTraversing = wander (\f (a, b) -> (\a' -> (a', b)) <$> f a)
 
 secondTraversing :: Traversing p => p a b -> p (c, a) (c, b)
 secondTraversing = traverse'
 
-swapE :: Either a b -> Either b a
-swapE = either Right Left
-
 leftTraversing :: Traversing p => p a b -> p (Either a c) (Either b c)
-leftTraversing = dimap swapE swapE . traverse'
+leftTraversing = wander (\f -> either (fmap Left . f) (pure . Right))
 
 rightTraversing :: Traversing p => p a b -> p (Either c a) (Either c b)
 rightTraversing = traverse'
@@ -82,6 +84,66 @@ instance Traversable (Baz t b) where
 instance Profunctor (Baz t) where
   dimap f g m = Baz $ \k -> runBaz m (fmap f . k . g)
 
+infixl 4 :<*>
+
+-- A list of values that have been traversed over so far. d is the input type;
+-- e is the planned output.
+data TList d e a where
+  TNil :: TList d e ()
+  (:<*>) :: d -> TList d e u -> TList d e (e, u)
+
+-- Trav is a Church-encoded free applicative, which is used to make traversing
+-- and assembling a TList faster by left-associating and bringing all the
+-- fmaps to the top. See https://www.eyrie.org/~zednenem/2013/06/freeapp-2 for
+-- details.
+newtype Trav d e a = Trav (forall u y z. (forall x. (x -> y) -> TList d e x -> z) -> (u -> a -> y) -> TList d e u -> z)
+
+instance Functor (Trav d e) where
+  {-# INLINE fmap #-}
+  fmap f (Trav m) = Trav $ \k s -> m k (\u -> s u . f)
+
+  {-# INLINE (<$) #-}
+  a <$ Trav m = Trav $ \k s -> m k (\u -> const $ s u a)
+
+instance Applicative (Trav d e) where
+  {-# INLINE pure #-}
+  pure a = Trav $ \k s -> k (flip s a)
+
+  {-# INLINE (<*>) #-}
+  Trav mf <*> Trav ma = Trav $ \k s -> ma (mf k) (\u a g -> s u (g a))
+
+-- Coyoneda encoding of a Functor.
+data Coyo f a where
+  Coyo :: (u -> a) -> f u -> Coyo f a
+
+-- Lift a d into an appropriate Trav with an unknown return type.
+{-# INLINE tLift #-}
+tLift :: d -> Trav d e e
+tLift d = Trav $ \k s p -> k (\ (a, u) -> s u a) (d :<*> p)
+
+-- Convert the Trav into an actual list.
+{-# INLINE runTrav #-}
+runTrav :: Trav d e a -> Coyo (TList d e) a
+runTrav (Trav m) = m Coyo (const id) TNil
+
+-- Split a Coyoneda-encoded TList into something an ArrowChoice can traverse.
+{-# INLINE unTList #-}
+unTList :: Coyo (TList d e) a -> Either a (d, Coyo (TList d e) (e -> a))
+unTList (Coyo f TNil) = Left (f ())
+unTList (Coyo f (d :<*> t)) = Right (d, Coyo (\u e -> f (e, u)) t)
+
+{-# INLINE wanderA #-}
+wanderA :: forall p a b s t. (ArrowChoice p)
+  => (forall f. (Applicative f) => (a -> f b) -> s -> f t)
+  -> p a b -> p s t
+wanderA tr p = go . arr (runTrav . tr tLift) where
+  go :: forall u. p (Coyo (TList a b) u) u
+  go = (id ||| arr (uncurry $ flip id) . (p *** go)) . arr unTList
+
+{-# INLINE traverseA #-}
+traverseA :: (ArrowChoice p, Traversable f) => p a b -> p (f a) (f b)
+traverseA = wanderA traverse
+
 -- | Note: Definitions in terms of 'wander' are much more efficient!
 class (Choice p, Strong p) => Traversing p where
   traverse' :: Traversable f => p a b -> p (f a) (f b)
@@ -109,6 +171,9 @@ instance Monad m => Traversing (Kleisli m) where
 instance Applicative m => Traversing (Star m) where
   traverse' (Star m) = Star (traverse m)
   wander f (Star amb) = Star (f amb)
+
+instance ArrowChoice p => Traversing (WrappedArrow p) where
+  wander f (WrapArrow p) = WrapArrow $ wanderA f p
 
 newtype CofreeTraversing p a b = CofreeTraversing { runCofreeTraversing :: forall f. Traversable f => p (f a) (f b) }
 
