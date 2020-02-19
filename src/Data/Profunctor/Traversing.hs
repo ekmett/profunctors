4620f750c12408827e240503af375073052036cf
guaraqe <mail@guaraqe.gq>
2020-02-19 15:08:07 +0100

    add a foldMapOf' function

--- src/Data/Profunctor/Traversing.hs
+++ src/Data/Profunctor/Traversing.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Data.Profunctor.Traversing
   ( Traversing(..)
   , CofreeTraversing(..)
@@ -9,6 +10,7 @@ module Data.Profunctor.Traversing
   -- * Folds in terms of Traversing
   , foldMap'
   , foldr'
+  , foldMapOf'
   -- * Profunctor in terms of Traversing
   , dimapWandering
   , lmapWandering
@@ -25,6 +27,7 @@ import Control.Applicative
 import Control.Arrow (Kleisli(..))
 import Data.Bifunctor.Tannen
 import Data.Functor.Compose
+import Data.Functor.Const (Const (..))
 import Data.Functor.Identity
 import Data.Monoid (Ap (..), Endo (..))
 import Data.Orphans ()
@@ -125,7 +128,7 @@ class (Choice p, Strong p) => Traversing p where
 
   {-# MINIMAL wander | traverse' #-}
 
--- | Fold efficiently a 'Traversing' profunctor.
+-- | Fold efficiently with a 'Traversing' profunctor.
 foldMap' :: (Traversing p, Foldable t, Monoid m) => p a m -> p (t a) m
 foldMap' = wander folder
   where
@@ -133,6 +136,16 @@ foldMap' = wander folder
 
 foldr' :: (Traversing p, Foldable t) => p a (b -> b) -> p (t a) (b -> b)
 foldr' = rmap appEndo . foldMap' . rmap Endo
+
+foldMapOf' ::
+  forall p m a s.
+  (Traversing p, Monoid m) =>
+  (forall r. Monoid r => (a -> Const r a) -> s -> Const r s) ->
+  p a m -> p s m
+foldMapOf' w = wander folder
+  where
+    folder :: (Applicative f, Monoid m) => (a -> f m) -> s -> f m
+    folder f = getAp . getConst . w (Const . Ap . f)
 
 instance Traversing (->) where
   traverse' = fmap
