f11bf940dc020e8543e22e77a96a0a6c2198dca6
Eitan Chatav <eitan.chatav@gmail.com>
2022-03-18 11:32:08 -0700

    introducing Lan
    * copy/paste Ran
    * rename Ran ~> Lan
    * minor doc adjudstments
    * define newtype Lan properly
    * define Profunctor (minus coercion) and Functor instances

---
+++ src/Data/Profunctor/Lan.hs
@@ -0,0 +1,135 @@
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE Safe #-}
+-----------------------------------------------------------------------------
+-- |
+-- Copyright   :  (C) 2013-2015 Edward Kmett and Dan Doel
+-- License     :  BSD-style (see the file LICENSE)
+--
+-- Maintainer  :  Edward Kmett <ekmett@gmail.com>
+-- Stability   :  provisional
+-- Portability :  Rank2Types, TFs
+--
+----------------------------------------------------------------------------
+module Data.Profunctor.Lan
+  ( Lan(..)
+  -- , decomposeLan
+  -- , precomposeLan
+  -- , curryLan
+  -- , uncurryLan
+  -- , Codensity(..)
+  -- , decomposeCodensity
+  ) where
+
+import Control.Category
+import Data.Profunctor
+-- import Data.Profunctor.Composition
+-- import Data.Profunctor.Functor
+-- import Data.Profunctor.Monad
+-- import Data.Profunctor.Unsafe
+import Prelude hiding (id,(.))
+
+--------------------------------------------------------------------------------
+-- * Lan
+--------------------------------------------------------------------------------
+
+-- | This represents the left Kan extension of a 'Profunctor' @q@ along a
+-- 'Profunctor' @p@ in a limited version of the 2-category of Profunctors where
+-- the only object is the category Hask, 1-morphisms are profunctors composed
+-- and compose with Profunctor composition, and 2-morphisms are just natural
+-- transformations.
+--
+-- 'Lan' has a polymorphic kind.
+
+-- Lan :: (k1 -> k2 -> Type) -> (k1 -> k3 -> Type) -> (k2 -> k3 -> Type)
+newtype Lan p q a b = Lan { runLan :: forall y. p b y -> q a y }
+
+-- instance Profunctor p => ProfunctorFunctor (Lan p) where
+--   promap f (Lan g) = Lan (f . g)
+
+-- instance (Profunctor p, Category p) => ProfunctorComonad (Lan p) where
+--   proextract (Lan f) = f id
+--   produplicate (Lan f) = Lan $ \ p -> Lan $ \q -> f (p . q)
+
+instance (Profunctor p, Profunctor q) => Profunctor (Lan p q) where
+  dimap ca bd f = Lan (lmap ca . runLan f . lmap bd)
+  {-# INLINE dimap #-}
+  lmap ca f = Lan (lmap ca . runLan f)
+  {-# INLINE lmap #-}
+  rmap bd f = Lan (runLan f . lmap bd)
+  {-# INLINE rmap #-}
+  -- bd #. f = Lan (\p -> bd #. runLan f p)
+  -- {-# INLINE (#.) #-}
+  -- f .# ca = Lan (\p -> runLan f (ca #. p))
+  -- {-# INLINE (.#) #-}
+
+instance Profunctor p => Functor (Lan p q a) where
+  fmap bd f = Lan (runLan f . lmap bd)
+  {-# INLINE fmap #-}
+
+-- -- | @'Lan' p p@ forms a 'Monad' in the 'Profunctor' 2-category, which is isomorphic to a Haskell 'Category' instance.
+-- instance p ~ q => Category (Lan p q) where
+--   id = Lan id
+--   {-# INLINE id #-}
+--   Lan f . Lan g = Lan (f . g)
+--   {-# INLINE (.) #-}
+
+-- -- | The 2-morphism that defines a left Kan extension.
+-- --
+-- -- Note: When @q@ is left adjoint to @'Lan' q (->)@ then 'decomposeLan' is the 'counit' of the adjunction.
+-- decomposeLan :: Procompose (Lan q p) q :-> p
+-- decomposeLan (Procompose (Lan qp) q) = qp q
+-- {-# INLINE decomposeLan #-}
+
+-- precomposeLan :: Profunctor q => Procompose q (Lan p (->)) :-> Lan p q
+-- precomposeLan (Procompose p pf) = Lan (\pxa -> runLan pf pxa `lmap` p)
+-- {-# INLINE precomposeLan #-}
+
+-- curryLan :: (Procompose p q :-> r) -> p :-> Lan q r
+-- curryLan f p = Lan $ \q -> f (Procompose p q)
+-- {-# INLINE curryLan #-}
+
+-- uncurryLan :: (p :-> Lan q r) -> Procompose p q :-> r
+-- uncurryLan f (Procompose p q) = runLan (f p) q
+-- {-# INLINE uncurryLan #-}
+
+-- --------------------------------------------------------------------------------
+-- -- * Codensity
+-- --------------------------------------------------------------------------------
+
+-- -- | This represents the right Kan extension of a 'Profunctor' @p@ along
+-- -- itself. This provides a generalization of the \"difference list\" trick to
+-- -- profunctors.
+-- --
+-- -- 'Codensity' has a polymorphic kind since @5.6@.
+
+-- -- Codensity :: (k1 -> k2 -> Type) -> (k2 -> k2 -> Type)
+-- newtype Codensity p a b = Codensity { runCodensity :: forall x. p x a -> p x b }
+
+-- instance Profunctor p => Profunctor (Codensity p) where
+--   dimap ca bd f = Codensity (rmap bd . runCodensity f . rmap ca)
+--   {-# INLINE dimap #-}
+--   lmap ca f = Codensity (runCodensity f . rmap ca)
+--   {-# INLINE lmap #-}
+--   rmap bd f = Codensity (rmap bd . runCodensity f)
+--   {-# INLINE rmap #-}
+--   bd #. f = Codensity (\p -> bd #. runCodensity f p)
+--   {-# INLINE (#.) #-}
+--   f .# ca = Codensity (\p -> runCodensity f (ca #. p))
+--   {-# INLINE (.#) #-}
+
+-- instance Profunctor p => Functor (Codensity p a) where
+--   fmap bd f = Codensity (rmap bd . runCodensity f)
+--   {-# INLINE fmap #-}
+
+-- instance Category (Codensity p) where
+--   id = Codensity id
+--   {-# INLINE id #-}
+--   Codensity f . Codensity g = Codensity (f . g)
+--   {-# INLINE (.) #-}
+
+-- decomposeCodensity :: Procompose (Codensity p) p a b -> p a b
+-- decomposeCodensity (Procompose (Codensity pp) p) = pp p
+-- {-# INLINE decomposeCodensity #-}
